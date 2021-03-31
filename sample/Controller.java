6300cecb01cc8efa85dec113db188c6b8f143000
MK <thedeadgamer101@gmail.com>
2021-03-31 09:30:18 +0300

    Created contact deletion and data update

--- sample/Controller.java
+++ sample/Controller.java
@@ -95,4 +95,27 @@ public class Controller {
             data.saveContacts();
         }
     }
+
+    public void showDeleteContactDialog() {
+        Contact selectedContact = contactsTable.getSelectionModel().getSelectedItem();
+        if (selectedContact == null){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("No contact selected!");
+            alert.setHeaderText(null);
+            alert.setContentText("I need you to select a contact before deletion!");
+            alert.showAndWait();
+            return;
+        }
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Delete Contact");
+        alert.setHeaderText(null);
+        alert.setContentText("Are you sure you want to delete the selected contact "+ selectedContact.getFirstName()+ " " + selectedContact.getLastName());
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.OK){
+            data.deleteContact(selectedContact);
+            data.saveContacts();
+        }
+    }
 }
